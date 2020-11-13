7610410408c82cde90e0889658cf97c41342f71a
Igor Muller <igor.muller87@gmail.com>
2020-11-13 10:56:06 -0300

    Ajuste Cors

--- backend/src/app/Http/Middleware/Cors.php
+++ backend/src/app/Http/Middleware/Cors.php
@@ -10,6 +10,6 @@ class Cors
     {
         return $next($request)->header('Access-Control-Allow-Origin', '*')
                               ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-                              ->header('Access-Control-Allow-Headers', 'Accept, X-XSRF-TOKEN, Access-Control-Allow-Headers, X-Requested-With, Content-Type, X-Auth-Token, Authorization, Origin');;
+                              ->header('Access-Control-Allow-Headers', 'Accept, X-XSRF-TOKEN, Access-Control-Allow-Headers, X-Requested-With, Content-Type, X-Auth-Token, Authorization, Origin');
     }
 }
